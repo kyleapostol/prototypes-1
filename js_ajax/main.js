fbbe6c39ade1ba107a23fd27af575ed22be935e9
kyle apostol <kyleapostol@yahoo.com>
2019-06-16 23:55:22 -0700

    JS | AJAX - Kyle Apostol

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,4 @@
-//Create GLOBAL variable below here on line 2
-
+var global_result;
 
 
 $(document).ready(initializeApp);
@@ -8,20 +7,34 @@ function initializeApp(){
     $('button').click(getData);
 }
 
-function getData(){
+function getData() {
     console.log('1) getData called from button click');
     var ajaxConfig = {
         dataType: 'json',
         url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
-        success: function(result) {
-            
-            console.log('2) AJAX Success function called, with the following result:', result);
+        method: 'get',
+        success: function (result) {
+            console.log('2) AJAX Success function called, with the following result:', result),
+                global_result = result;
 
+            var firstMovieThirdImage = result.feed.entry[0]['im:image'][2].label;
+            console.log('firstMovieThirdImage: ', firstMovieThirdImage);
+
+            for (var movieImages = 0; movieImages < result.feed.entry.length; movieImages++) {
+                var thirdMovieImage = result.feed.entry[movieImages]['im:image'][2].label;
+                var image = $('<img>').attr('src', thirdMovieImage);
+                var title = result.feed.entry[movieImages].title.label;
+                console.log("grab the title: ", title);
+                var text = $('<p>').text(title);
+                 $('#main').append(image);
+                 $('#main').append(text);
+            }
         }
+        
     }
 
-    console.log('3) Making AJAX request');
-    $.ajax(ajaxConfig);
+            console.log('3) Making AJAX request');
+            $.ajax(ajaxConfig);
 
-    console.log('4) End of getData');
+            console.log('4) End of getData');
 }
